823726bb467d531775c04c501cb7506f71d9892e
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-03-27 03:33:42 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,120 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int[] bubbleSort(double[] nums) {
+		boolean swapped = true;
+		int swaps = 0;
+		int comparisons = 0;
+		for (int i = 0; i < nums.length - 1 && swapped; i++) {
+			swapped = false;
+			for (int e = 0; e < nums.length - 1 - i; e++) {
+				comparisons++;
+				if (nums[e] > nums[e + 1]) {
+					swaps += 3;
+					double temp = nums[e];
+					nums[e] = nums[e + 1];
+					nums[e + 1] = temp;
+					swapped = true;
+				}
+			}
+		}
+		return new int[]{swaps, comparisons};
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static int[] selectionSort(double[] nums) {
+		int swaps = 0;
+		int comparisons = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			int currMin = i;
+			for (int e = i + 1; e < nums.length; e++) {
+				comparisons++;
+				if (nums[e] < nums[currMin]) {
+					swaps++;
+					currMin = e;
+				}
+			}
+			swaps += 3;
+			double temp = nums[i];
+			nums[i] = nums[currMin];
+			nums[currMin] = temp;
+		}
+		return new int[]{swaps, comparisons};
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static int[] insertionSort(double[] nums) {
+		int swaps = 0;
+		int comparisons = 0;
+		for (int i = 1; i < nums.length; i++) {
+			swaps++;
+			double current = nums[i];
+			int index = i - 1;
+			while (index >= 0 && nums[index] > current) {
+				comparisons++;
+				nums[index + 1] = nums[index];
+				swaps++;
+				index--;
+			}
+			nums[index + 1] = current;
+			swaps++;
+		}
+		return new int[]{swaps, comparisons};
 	}
 
-	public static void q4() {
-		//Write question 4 code here
+	public static String leastSwaps(double[] nums) {
+		double[] copy = new double[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			copy[i] = nums[i];
+		}
+		int bubbleResult = bubbleSort(copy)[0];
+		for (int i = 0; i < nums.length; i++) {
+			copy[i] = nums[i];
+		}
+		int selectionResult = selectionSort(copy)[0];
+		for (int i = 0; i < nums.length; i++) {
+			copy[i] = nums[i];
+		}
+		int insertionResult = insertionSort(copy)[0];
+		if (bubbleResult <= selectionResult) {
+			if (bubbleResult <= insertionResult) {
+				return "Bubble";
+			} else {
+				return "Insertion";
+			}
+		} else {
+			if (selectionResult < insertionResult) {
+				return "Selection";
+			} else {
+				return "Insertion";
+			}
+		}
 	}
 
-	public static void q5() {
-		//Write question 5 code here
+	public static String leastIterations(double[] nums) {
+		double[] copy = new double[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			copy[i] = nums[i];
+		}
+		int bubbleResult = bubbleSort(copy)[1];
+		for (int i = 0; i < nums.length; i++) {
+			copy[i] = nums[i];
+		}
+		int selectionResult = selectionSort(copy)[1];
+		for (int i = 0; i < nums.length; i++) {
+			copy[i] = nums[i];
+		}
+		int insertionResult = insertionSort(copy)[1];
+		if (bubbleResult <= selectionResult) {
+			if (bubbleResult <= insertionResult) {
+				return "Bubble";
+			} else {
+				return "Insertion";
+			}
+		} else {
+			if (selectionResult < insertionResult) {
+				return "Selection";
+			} else {
+				return "Insertion";
+			}
+		}
 	}
-
 }
